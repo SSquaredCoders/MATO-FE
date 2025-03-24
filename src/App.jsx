4e7e71574b149b189a62efedfb9a59615fb65996
JEESW <jeesw1221@gmail.com>
2025-03-25 02:13:36 +0900

    feat: 방 목록 조회 페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Logout from "./Logout.jsx";
 import Register from "./Register.jsx";
 import UpdateUser from "./UpdateUser.jsx";
 import DeleteUser from "./DeleteUser.jsx";
+import RoomListPage from "./RoomListPage.jsx";
 import ChatRoom from "./ChatRoom.jsx";
 import CreateMap from "./CreateMap.jsx";
 import MapList from "./MapList.jsx";
@@ -28,6 +29,7 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/update-user" element={<UpdateUser />} />
                 <Route path="/delete-user" element={<DeleteUser />} />
+                <Route path="/rooms" element={<RoomListPage />} />
                 <Route path="/room/:roomName" element={<ChatRoom />} />
                 <Route path="/create-map" element={<CreateMap />} />
                 <Route path="/map-list" element={<MapList />} />
