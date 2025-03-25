d64e4939b187d10fd5fb0d0b025514399cf9194b
LLSNsssz <seungjang93@gmail.com>
2025-03-26 02:05:55 +0900

    Feat: 타입 추가

--- src/types/song.ts
+++ src/types/song.ts
@@ -1,4 +1,14 @@
-export interface SongRequestDto {
-    youtubeUrl: string;
+import {AnswerItem} from "./answer";
+import {HintItem} from "./hint";
+
+export type SongItem = {
+    id: number; // MapSong ID
+    songId: number; // 실제 Song ID
     title: string;
-}
+    youtubeUrl: string;
+    startTime: number;
+    endTime: number;
+    repeatCount: number;
+    answers: AnswerItem[];
+    hints: HintItem[];
+};
