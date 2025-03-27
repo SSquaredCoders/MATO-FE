742eeb0bcde89fb18b53db1443aaaa0c6ea7918e
LeeSH <seungjang93@gmail.com>
2025-03-28 01:46:15 +0900

    Chore: 힌트 관련 타입 정리 및 백엔드 DTO 대응 타입 정의 추가

--- src/types/hint.ts
+++ src/types/hint.ts
@@ -3,3 +3,27 @@ export type HintItem = {
     text: string;
     revealTime: number;
 };
+
+export interface HintDto {
+    id: number;
+    mapSongId: number;
+    hintText: string;
+    hintTime: number;
+}
+
+export interface HintData {
+    hintText: string;
+    revealTime: number;
+}
+
+export interface HintRequest {
+    mapSongId: number;
+    hints: HintData[];
+}
+
+export interface HintResponse {
+    id: number;
+    mapSongId: number;
+    hintText: string;
+    revealTime: number;
+}
